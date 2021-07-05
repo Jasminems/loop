ce28358b9fa586da0dfc7445f1b1717ada12db84
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 23:04:07 +0530

    While

---
+++ Doprint/src/doprin.java
@@ -0,0 +1,14 @@
+/*A Java program to print numbers from 1 to 10 using do-while loop*/
+class doprin 
+{
+	public static void main(String[] args) 
+	{
+		int i=1;
+		System.out.println("Numbers from 1 to 10:");
+		do
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+	}
+}
