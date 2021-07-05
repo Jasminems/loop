b5be7a5b4e59459649b27791583649b59300c690
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 22:38:59 +0530

    For Loop

---
+++ Floop/src/Fl.java
@@ -0,0 +1,12 @@
+/*A Java program to print "Bright IT Career" ten times using for loop*/
+class Fl
+{
+	public static void main(String[] args) 
+	{
+		String n="Bright IT Career";
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n);
+		}
+	}
+}
