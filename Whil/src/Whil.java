85aac0a997e67e94897da61d8898ba474bd7993e
Jasminems <jasmine.srakj@gmail.com>
2021-07-05 22:43:04 +0530

    While

---
+++ Whil/src/Whil.java
@@ -0,0 +1,14 @@
+/*A Java program to print numbers from 1 to 20 using while loop*/
+class Whil 
+{
+	public static void main(String[] args) 
+	{
+		int i=1;
+		System.out.println("The Numbers from 1 to 20:");
+		while(i<=20)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
